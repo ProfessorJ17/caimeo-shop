0ccc2d26f10d91e3e5e904ee3a05876064403ef1
ProfessorJ17 <162835589+ProfessorJ17@users.noreply.github.com>
2025-07-06 04:03:53 -0500

    Create firebase.js

---
+++ orion/firebase.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDYXyAmDC_HF4scI21VeLlS4aM2k2RJ1Wc",
+  authDomain: "mainframe-e3b13.firebaseapp.com",
+  projectId: "mainframe-e3b13",
+  storageBucket: "mainframe-e3b13.appspot.com",
+  messagingSenderId: "85561924382",
+  appId: "1:85561924382:web:5d5eddda04a466ec56870f",
+  measurementId: "G-N2Q017PZXB"
+};
